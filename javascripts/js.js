97fea2986b48ffe664957eafb61b94d731ebd95e
Alex Lang <langalex4@gmail.com>
2014-11-27 12:29:03 -0600

    redesign finished, i hope

--- javascripts/js.js
+++ javascripts/js.js
@@ -1,11 +1,11 @@
 
 $(function() {
 
-	$.each($('.js-slide'), function(i, val) {
-		if ($(this).attr('id') != '1') {$(this).hide();}
+	$('#pagepiling').pagepiling({
+		scrollingSpeed: 300,
+		verticalCentered: true,
+		touchSensitivity: 50
 	});
-
-	
 
 });
 
